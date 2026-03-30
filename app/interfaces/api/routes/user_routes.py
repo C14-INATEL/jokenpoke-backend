cc62fd409e6d5d3da61e559eee0ce975b42bda33
Bagrielzin <gabriel.kratos2002@gmail.com>
2026-03-30 19:09:49 -0300

    refactor: modifica padrão de resposta do get_all_users

--- app/interfaces/api/routes/user_routes.py
+++ app/interfaces/api/routes/user_routes.py
@@ -8,7 +8,12 @@ from app.schemas.user_schema import UserResponse
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.get("/", response_model=List[UserResponse])
+@router.get("/", response_model_exclude={
+        "__all__": {
+            "collection": {"__all__": {"description"}},
+            "deck": {"__all__": {"description"}}
+        }
+    })
 def get_all_users(db: Session = Depends(get_db)):
     use_case = GetAllUsersUseCase(db)
     users = use_case.execute()
