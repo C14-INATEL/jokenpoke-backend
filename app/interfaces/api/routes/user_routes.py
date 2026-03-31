6ae43cf9e99b82fed1625e842c7694608d870a8a
Bagrielzin <gabriel.kratos2002@gmail.com>
2026-03-30 21:09:03 -0300

    feat:adiciona rota de delete

--- app/interfaces/api/routes/user_routes.py
+++ app/interfaces/api/routes/user_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 
+from app.application.use_cases.delete_user import DeleteUserUseCase
 from app.application.use_cases.get_user_by_id import GetUserByIdUseCase
 from app.infrastructure.db.session import get_db
 from app.application.use_cases.get_all_users import GetAllUsersUseCase
@@ -31,3 +32,10 @@ def get_all_users(db: Session = Depends(get_db)):
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     use_case = GetUserByIdUseCase(db)
     return use_case.execute(user_id)
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    use_case = DeleteUserUseCase(db)
+    deleted_username = use_case.execute(user_id)
+    
+    return {"message": f"Usuário {deleted_username} deletado com sucesso"}
