01e46ca5a71164e6f3172ff955830ffcd0d731e0
Bagrielzin <gabriel.kratos2002@gmail.com>
2026-03-30 22:04:12 -0300

    feat: adiciona request do build deck

--- app/schemas/deck_schema.py
+++ app/schemas/deck_schema.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+from typing import List
 
 class DeckResponse(BaseModel):
     id: int
@@ -6,3 +7,6 @@ class DeckResponse(BaseModel):
     card_id: int
 
     model_config = ConfigDict(from_attributes=True)
+
+class BuildDeckRequest(BaseModel):
+    pokemon_ids: List[int] = Field(..., min_length=3, max_length=3, description="Lista com exatamente 3 IDs de Pokémons")
