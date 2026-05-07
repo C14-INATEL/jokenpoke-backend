c55860ec32227b761f59151951fb10233f4bd1c9
odraude222k <pereirabentoeduardo@gmail.com>
2026-05-07 09:37:06 -0300

    feat: adiciona lógica de subida e descida de rank

---
+++ app/domain/rules/rank_up_down.py
@@ -0,0 +1,15 @@
+RANK_UP = {
+    "Beginner" : "Great",
+    "Great" : "Expert",
+    "Expert" : "Veteran",
+    "Veteran" : "Ultra",
+    "Ultra" : "Master",
+}
+
+RANK_DOWN = {
+    "Master" : "Ultra",
+    "Ultra" : "Veteran",
+    "Veteran" : "Expert",
+    "Expert" : "Great",
+    "Great" : "Beginner",
+}
