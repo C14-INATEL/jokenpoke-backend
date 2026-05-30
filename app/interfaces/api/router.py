94ed02695bdee4d45b48db6a56fa059024eb3b36
RafaelAreias <a.silveira.rafael@gmail.com>
2026-05-30 19:31:20 -0300

    feat(ranking): inclui ranking routes no roteador da api

--- app/interfaces/api/router.py
+++ app/interfaces/api/router.py
@@ -4,6 +4,7 @@ from app.interfaces.api.routes import (
     auth_routes,
     card_routes,
     deck_routes,
+    ranking_routes,
     user_routes,
 )
 
@@ -13,5 +14,5 @@ router.include_router(auth_routes.router)
 # router.include_router(battle_routes.router)
 router.include_router(card_routes.router)
 router.include_router(deck_routes.router)
-# router.include_router(ranking_routes.router)
+router.include_router(ranking_routes.router)
 router.include_router(user_routes.router)
