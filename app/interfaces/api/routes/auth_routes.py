81fa95f375114af5b6a84e7310b4a60f11a1138d
mariaclaraig <mclara.ignacio@gmail.com>
2026-05-31 21:36:06 -0300

    feat(auth): implement login endpoint with JWT authentication

--- app/interfaces/api/routes/auth_routes.py
+++ app/interfaces/api/routes/auth_routes.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.application.use_cases.login_user import LoginUserUseCase
 from app.application.use_cases.register_user import RegisterUserUseCase
 from app.infrastructure.db.session import get_db
-from app.schemas.auth_schema import RegisterUserRequest, RegisterUserResponse
+from app.schemas.auth_schema import (
+    LoginUserRequest,
+    LoginUserResponse,
+    RegisterUserRequest,
+    RegisterUserResponse,
+)
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -17,3 +23,11 @@ def register(payload: RegisterUserRequest, db: Session = Depends(get_db)):
     token = use_case.execute(username=payload.username, password=payload.password)
 
     return {"message": "Usuário registrado com sucesso", "access_token": token}
+
+
+@router.post("/login", response_model=LoginUserResponse)
+def login(payload: LoginUserRequest, db: Session = Depends(get_db)):
+    use_case = LoginUserUseCase(db)
+    token = use_case.execute(username=payload.username, password=payload.password)
+
+    return {"message": "Login realizado com sucesso", "access_token": token}
