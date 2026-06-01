f0312ecfc5e7328e3cf8b62ce8acdbfb011d7247
Bagrielzin <gabriel.kratos2002@gmail.com>
2026-05-31 22:49:20 -0300

    fix(security): fix hash_passoword

--- app/infrastructure/security/password.py
+++ app/infrastructure/security/password.py
@@ -4,7 +4,10 @@ pwd_context = CryptContext(schemes=["bcrypt"])
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    password_bytes = password.encode("utf-8")
+    if len(password_bytes) > 72:
+        password_bytes = password_bytes[:72]
+    return pwd_context.hash(password_bytes)
 
 
 def verify_password(password: str, hashed: str) -> bool:
