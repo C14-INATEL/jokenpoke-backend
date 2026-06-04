adc88012cec23bd2ecbd64537af791602c67495a
MatheusNetto1 <netto.matheus123@outlook.com>
2026-06-03 22:39:22 -0300

    fix: add ondelete cascade to deck and cards FKs, fix ranking column migration

---
+++ alembic/versions/9ed4e933c720_add_cascade_delete_to_deck_user_fk.py
@@ -0,0 +1,36 @@
+"""add_cascade_delete_to_deck_user_fk
+
+Revision ID: 9ed4e933c720
+Revises: fb4eac9f3c78
+Create Date: 2026-06-03 21:54:31.114243
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '9ed4e933c720'
+down_revision: Union[str, Sequence[str], None] = 'fb4eac9f3c78'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_constraint('deck_user_id_fkey', 'deck', type_='foreignkey')
+    op.create_foreign_key(
+        'deck_user_id_fkey',
+        'deck', 'users',
+        ['user_id'], ['id'],
+        ondelete='CASCADE'
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint('deck_user_id_fkey', 'deck', type_='foreignkey')
+    op.create_foreign_key(
+        'deck_user_id_fkey',
+        'deck', 'users',
+        ['user_id'], ['id']
+    )
---
+++ alembic/versions/d8d5eb44c880_add_cascade_delete_to_cards_user_fk.py
@@ -0,0 +1,36 @@
+"""add_cascade_delete_to_cards_user_fk
+
+Revision ID: d8d5eb44c880
+Revises: 9ed4e933c720
+Create Date: 2026-06-03 22:10:11.585829
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'd8d5eb44c880'
+down_revision: Union[str, Sequence[str], None] = '9ed4e933c720'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_constraint('cards_owner_id_fkey', 'cards', type_='foreignkey')
+    op.create_foreign_key(
+        'cards_owner_id_fkey',
+        'cards', 'users',
+        ['owner_id'], ['id'],
+        ondelete='CASCADE'
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint('cards_owner_id_fkey', 'cards', type_='foreignkey')
+    op.create_foreign_key(
+        'cards_owner_id_fkey',
+        'cards', 'users',
+        ['owner_id'], ['id']
+    )
